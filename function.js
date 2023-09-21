b10d9f7b8afc07e7600815ddb04d740b1f894fc8
dhananjaya5023 <dhananjaya5023@gmail.com>
2023-09-21 12:53:19 -0400

    functions

---
+++ function.js
@@ -0,0 +1,16 @@
+window.confirm("This is secret don't publish")
+document.write("<h2>Accounts</h2>");
+document.write("<p>Ever wondered what happens to your money after your death? There must be many questions in your mind related to your money and savings after you are gone. Firstly, have you designated a nominee for your bank account? Ensuring a nominee is essential for a smooth transition of your savings to loved ones in the event of your passing. </p>");
+document.write("<h3> Dhananjaya Rol</h3>");
+document.write("<table> <tr><td>ROL</td><td>DAYS </td></tr>  <tr><td>Front end developer</td><td>15days </td></tr> <tr><td>Back end devoloper</td><td>8 days </td></tr>  </table>");
+
+document.getElementById("name").innerHTML="Dhananjaya";
+
+var name1=window.prompt("Plece Enter your name");
+document.getElementById("veera").innerHTML='Wellcome!' + name1 ;
+
+document.write("<h3> Vivek Rol</h3>");
+document.write("<table> <tr><td>ROL</td><td>DAYS </td></tr>  <tr><td>Front end developer</td><td>12days </td></tr> <tr><td>Back end devoloper</td><td>11 days </td></tr>  </table>");
+
+document.write("<button> Click me  </button>")
+
